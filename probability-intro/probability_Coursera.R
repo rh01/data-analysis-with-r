1062d7eaebf168a21e957f126c593e1e86588bf8
rh01 <1048157315@qq.com>
2017-10-14 23:22:16 +0800

    bak

---
+++ probability-intro/probability_Coursera.R
@@ -0,0 +1,17 @@
+
+
+library(ggplot2)
+library(statsr)
+library(dplyr)
+
+data("kobe_basket")
+dim(kobe_basket)
+names(kobe_basket)
+show(kobe_basket)
+
+# show data structure
+str(kobe_basket)
+
+head(kobe_basket)
+
+
